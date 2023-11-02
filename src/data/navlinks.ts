9406c61b9e8f472ed03c95bc55a31f662a5059eb
Foparty <alfonsomartingarcia@gmail.com>
2023-11-02 11:55:10 +0100

    last changes

---
+++ src/data/navlinks.ts
@@ -0,0 +1,27 @@
+export const navigation = [
+	{
+		id: 1,
+		title: 'work',
+		url: '/'
+	},
+	{
+		id: 2,
+		title: 'words',
+		url: '/words'
+	},
+	{
+		id: 3,
+		title: 'about',
+		url: '/about'
+	},
+	{
+		id: 4,
+		title: 'info',
+		url: '/info'
+	},
+	{
+		id: 5,
+		title: '...',
+		url: '/more'
+	},
+]
